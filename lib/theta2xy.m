c29f09666a2fa1fbedb04c6b30d12033fd75731e
Kimssu <99416408+Kimssu@users.noreply.github.com>
2023-10-27 12:08:22 +0900

    Delete lib directory

--- lib/theta2xy.m
+++
@@ -1,8 +0,0 @@
-function [X, Y] = theta2xy(T, E)
-	if ~exist('E', 'var') || isempty(E)
-		E = ones(size(T));
-	end
-
-	X = cosd(T - 90) .* E;
-	Y = sind(T - 90) .* E;
-end
--- lib/xy2theta.m
+++
@@ -1,3 +0,0 @@
-function T = xy2theta(X, Y)
-	T = mod(atan2d(Y, X) + 90, 360);
-end
